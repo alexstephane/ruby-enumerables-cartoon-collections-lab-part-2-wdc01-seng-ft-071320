89f26685f46929b86e7af77110001a5ba625a1f3
Alex Ngundji <alexngundji@gmail.com>
2020-06-24 18:02:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -32,7 +32,7 @@ end
 
 
 
-# def find_valid_calls(planeteer_calls)
+ def find_valid_calls(planeteer_calls)
 #   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
 #   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
 #   # Return the first valid call found, or return nil if no valid calls are found
@@ -40,7 +40,7 @@ end
  
 # end
 
-def find_the_cheese(cheese)# code an argument here
+# def find_the_cheese(cheese)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese.find do |cheese|
